02163ca2271e38f367e0a1ad8fe6e4478c5464ac
Braxton Walters <braxton.walters@gmail.com>
2023-06-29 18:43:03 -0400

    add projects animation

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const anElement = document.querySelectorAll(".anOnScroll")
+let nodeList = document.querySelectorAll(".anOnScroll")
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry => {
+    entries.forEach(entry => {
         if (entry.isIntersecting) {
-            entry.target.classList.add("")
+            entry.target.classList.add('animate');
+            console.log(entry.target.classList);
         }
-    }))
+    })
 }, {
     threshold: 0.5
 });
 
-for (let i = 0; i > anElement.length; i++) {
-    const element = anElement[i];
-    observer.observe[element];
+for (let i = 0; i < nodeList.length; i++) {
+    const element = nodeList[i];
+    console.log(nodeList[i]);
+    observer.observe(element);
 }
+
